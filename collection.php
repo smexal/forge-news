56c26aa69dca556d56404cf181021d9afa29bb4d
smexal <silas.maechler@gmail.com>
2016-06-05 01:18:52 +0200

    update collection setup

--- collection.php
+++ collection.php
@@ -3,9 +3,11 @@ class ForgeNewsCollection extends DataCollection {
   public $permission = "manage.collection.sites";
 
   protected function setup() {
-    $this->preferences['title'] = i('News');
-    $this->preferences['all-title'] = i('Manage News');
-    $this->preferences['add-label'] = i('Add news');
+    $this->preferences['name'] = 'forge-news';
+    $this->preferences['title'] = i('News', 'forge-news');
+    $this->preferences['all-title'] = i('Manage News', 'forge-news');
+    $this->preferences['add-label'] = i('Add news', 'forge-news');
+    $this->preferences['single-item'] = i('News', 'forge-news');
   }
 }
 
