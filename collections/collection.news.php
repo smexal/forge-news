7e35d9dc2d187ba70977bff9e7e9148483f8b85d
s.lehmann@p-art-s.ch <simu1990>
2017-09-16 21:01:52 +0200

    disabled categories for datacollections per default added categories to news

--- collections/collection.news.php
+++ collections/collection.news.php
@@ -16,6 +16,7 @@ class NewsCollection extends DataCollection {
     $this->preferences['all-title'] = i('Manage News', 'forge-news');
     $this->preferences['add-label'] = i('Add news', 'forge-news');
     $this->preferences['single-item'] = i('News', 'forge-news');
+    $this->preferences['categories'] = true;
 
     $this->custom_fields();
   }
